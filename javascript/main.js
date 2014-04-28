257df12bfbc28b5d94e680b63295fdc5ce888a6b
evabrandenburg <evabrandenburg89@gmail.com>
2014-04-28 16:32:33 -0400

    ugly random background!

--- javascript/main.js
+++ javascript/main.js
@@ -6,3 +6,20 @@ $(document).ready(function(){
   	slideMargin: 10
 	});
 });
+
+var changePic = $( '.more' );
+var rando = Math.random();
+var pix = $( '.container' );
+changePic.click(
+	'click'
+	, function( e ) {
+		
+		if(rando < 0.5) { 
+			pix.addClass( 'choiceOne' );
+		}
+		else {
+			pix.addClass( 'choiceTwo' );
+		}
+	});
+
+
