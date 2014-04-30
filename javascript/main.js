2f3a9dc93c2765f70f91b85586dfe396bcf5a096
evabrandenburg <evabrandenburg89@gmail.com>
2014-04-29 20:08:28 -0400

    working random

--- javascript/main.js
+++ javascript/main.js
@@ -1,27 +1,38 @@
+(function(){
+
 $(document).ready(function(){
-  $('.bxslider').bxSlider({
+  /*$('.bxslider').bxSlider({
   	minSlides: 1,
   	maxSlides: 1,
   	slideWidth: 350,
   	slideMargin: 10
+	});*/
+
+var changePic = $( '.more' );
+var pix = $( '.container' );
+
+changePic.click(
+	function( e ) {
+
+
+		var rando = Math.random();
+		
+		console.log( rando );
+
+		if(rando < 0.5) { 
+			pix.attr('class', '').addClass( 'container choiceOne' );
+			//pix.addClass( 'choiceOne' );
+		}
+		else {
+			pix.attr('class', '').addClass( 'container choiceTwo' );
+			//pix.addClass( 'choiceTwo' );
+		}
+
+
 	});
 });
 
-var changePic = $( '.more' );
-var rando = Math.random();
-var pix = $( '.container' );
-changePic.click(
-	'click'
-	, function( e ) {
-		
-		if(rando < 0.5) { 
-			pix.addClass( 'choiceOne' );
-		}
-		else {
-			pix.addClass( 'choiceTwo' );
-		}
 
-		alert( val('rando') );
-	});
+})();
 
 
